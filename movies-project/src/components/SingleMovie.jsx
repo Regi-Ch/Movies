797446d3d33125e890c4f2501bdc0f3165ab43b6
Regi-Ch <regi_ch6@hotmail.com>
2024-03-08 20:56:52 -0600

    Avance 3

--- movies-project/src/components/SingleMovie.jsx
+++ movies-project/src/components/SingleMovie.jsx
@@ -12,8 +12,9 @@ const SingleMovie = () => {
         return <div className="loading"></div>
     }
 
-    const { Poster, Title, Plot, Year, Country, Director, Released, Runtime } = data;
+    const { Poster, Title, Plot, Year, Country, Director, Released, Runtime, Actors, Rated, Genre } = data;
     let image = Poster === "N/A" ? DefaultImage : Poster;
+    //console.log(data);
 
     return(
         !isLoading ?
@@ -22,11 +23,13 @@ const SingleMovie = () => {
             <div className="single-info">
                 <h2>{ Title }</h2>
                 <p>{ Plot }</p>
-                <p><strong>Country: </strong>{ Country }</p>
                 <p><strong>Director: </strong>{ Director }</p>
-                <p><strong>Released: </strong>{ Released }</p>
+                <p><strong>Actors: </strong>{ Actors }</p>
+                <p><strong>Genre: </strong>{ Genre }</p>
+                <p><strong>Rated: </strong>{ Rated }</p>
                 <p><strong>Runtime: </strong>{ Runtime }</p>
-                <p><strong>Year: </strong>{ Year }</p>
+                <p><strong>Released: </strong>{ Released }</p>
+                <p><strong>Country: </strong>{ Country }</p>
             </div>
         </div>
         : ''
